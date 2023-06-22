33555161d1c81c1946e3a26931daf9a5b15ee793
olinaf11 <fanilokevin@icloud.com>
2023-06-22 21:03:29 +0300

    modif sql

--- schema.sql
+++ schema.sql
@@ -93,17 +93,6 @@ CREATE TABLE tarif (
 );
 
 CREATE OR REPLACE VIEW v_liste_prevision_a_venir AS
-SELECT * 
-FROM prevision 
+SELECT *
+FROM prevision
 WHERE arrive > NOW();
-
-CREATE OR REPLACE VIEW v_tarif_quai AS
-SELECT idTarif, idQuai, detail.idPrestation, idType, idPavillon, tra.debut, tra.fin, prix
-FROM tarif tar
-    JOIN detailtarif detail ON tar.idDetailTarif = detail.idDetailTarif
-    JOIN tranche tra ON tra.tranche = tar.tranche;
-
-SELECT 'TAR001' as idTarif, idquai, idPrestation, idType, idPavillon, 0 as debut, 0 as fin, SUM(prix)
-FROM v_tarif_quai
-WHERE idquai='QUA001' AND idPrestation='PRES001' AND idType='TYP001' AND idPavillon='PAV01' AND debut < 40
-GROUP BY idquai, idPrestation, idType, idPavillon;
