54799bba80ae482708e346e651309fb66665febf
ManoarySarobidy <94636671+Tendrynyavo@users.noreply.github.com>
2023-06-26 12:34:35 +0300

    escale query

--- WEB-INF/classes/Escale.java
+++ WEB-INF/classes/Escale.java
@@ -1,6 +1,8 @@
 package escale;
 
 import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
@@ -103,7 +105,7 @@ public class Escale extends Proposition {
     }
 
     public Escale[] findAll(Connection connection, String order) throws Exception {
-        String sql = "select * from v_escale";
+        String sql = "SELECT * FROM v_escale";
         ArrayList<Escale> escales = new ArrayList<Escale>();
         java.sql.Statement st = connection.createStatement();
         java.sql.ResultSet set = st.executeQuery( sql );
@@ -133,9 +135,9 @@ public class Escale extends Proposition {
     }
 
     public static Escale getByReference( Connection connection, String reference ) throws Exception{
-        String sql = "select * from v_escale where reference='%s'";
-        java.sql.Statement st = connection.createStatement();
-        java.sql.ResultSet set = st.executeQuery( String.format(sql, reference) );
+        String sql = "SELECT * FROM v_escale WHERE reference='%s'";
+        Statement st = connection.createStatement();
+        ResultSet set = st.executeQuery( String.format(sql, reference) );
         set.next();
         Escale escale = new Escale(set.getString("reference"), set.getTimestamp("debut"), set.getTimestamp("fin"), set.getDouble("cours"));
         escale.setBateau( set.getString("idBateau") , connection);
@@ -165,10 +167,9 @@ public class Escale extends Proposition {
         return escale;
     }
 
-    public Facture facturer() throws Exception{
+    public Facture facturer() throws Exception {
         // if (this.enCours()) throw new Exception("Escale est encore en cours");
         Facture facture = new Facture();
-        
         Vector<Facture> factures = new Vector<>();
         for (Prestation prestation : this.getListePrestation()) {
             if (prestation.getEtat() >= 10) {
@@ -183,8 +184,8 @@ public class Escale extends Proposition {
     public Prestation[] getPrestations(Connection connection, String quai) throws Exception {
         String sql = "SELECT * FROM v_escale_prestation WHERE id_quai='%s' AND reference='%s'";
         ArrayList<Prestation> prestations = new ArrayList<>();
-        java.sql.Statement st = connection.createStatement();
-        java.sql.ResultSet set = st.executeQuery( String.format(sql, quai, this.getReference()) );
+        Statement st = connection.createStatement();
+        ResultSet set = st.executeQuery( String.format(sql, quai, this.getReference()) );
         while( set.next() ) {
             Prestation prestation = new Prestation(set.getString("id_prestation"), set.getString("nom"), set.getTimestamp("debut"), set.getTimestamp("fin"), set.getDouble("prix"), set.getInt("etat"), this);
             prestation.setId(set.getString("id_escale_prestation"));
@@ -197,8 +198,8 @@ public class Escale extends Proposition {
     public Prestation[] getPrestations(Connection connection) throws Exception {
         String sql = "SELECT * FROM v_escale_prestation WHERE reference='%s'";
         ArrayList<Prestation> prestations = new ArrayList<>();
-        java.sql.Statement st = connection.createStatement();
-        java.sql.ResultSet set = st.executeQuery( String.format(sql, this.getReference()) );
+        Statement st = connection.createStatement();
+        ResultSet set = st.executeQuery( String.format(sql, this.getReference()) );
         while( set.next() ) {
             Escale escale = new Escale();
             escale.setReference(set.getString("reference"));
@@ -214,14 +215,11 @@ public class Escale extends Proposition {
     public Prestation getById(Connection connection, String id) throws Exception {
         boolean open = false;
         if ( connection == null ) { connection = BddObject.getPostgreSQL(); open = true; }
-        String sql = "SELECT * FROM v_escale_prestation WHERE id_escale_prestation='%s'";
-        ArrayList<Prestation> prestations = new ArrayList<Prestation>();
-        java.sql.Statement st = connection.createStatement();
-        java.sql.ResultSet set = st.executeQuery(String.format(sql, id));
+        Statement st = connection.createStatement();
+        ResultSet set = st.executeQuery(String.format("SELECT * FROM v_escale_prestation WHERE id_escale_prestation='%s'", id));
         set.next();
         Prestation prestation = new Prestation(set.getString("id_prestation"), set.getString("nom"), set.getTimestamp("debut"), set.getTimestamp("fin"), set.getDouble("prix"), set.getInt("etat"), this);
-        prestation.setId(set.getString("id_escale_prestation"));
-        prestation.setEscale(this);        
+        prestation.setId(set.getString("id_escale_prestation"));    
         st.close();
         set.close();
         if ( open ) { connection.commit(); connection.close(); }
