8064469aa8cd84811293031d3dd31f9082f9af8b
ManoarySarobidy <94636671+Tendrynyavo@users.noreply.github.com>
2023-06-26 12:50:08 +0300

    fin escale

--- WEB-INF/classes/FinEscale.java
+++ WEB-INF/classes/FinEscale.java
@@ -7,19 +7,17 @@ import connection.BddObject;
 import connection.annotation.ForeignKey;
 import connection.annotation.PrimaryKey;
 import connection.annotation.ColumnName;
-
 import escale.Escale;
-
 import prevision.Prevision;
 
-public class FinEscale extends BddObject<DebutEscale>{
+public class FinEscale extends BddObject<FinEscale> {
+    
     @ColumnName("id_fin")
     String idFin;
     @ColumnName("id_debut")
     String idDebut;
     Timestamp fin;
     double cours;
-
 
     public FinEscale() throws Exception{
         this.setTable("fin_escale");
@@ -28,6 +26,7 @@ public class FinEscale extends BddObject<DebutEscale>{
         this.setPrefix("FES");
         this.setConnection("PostgreSQL");
     }
+
     public FinEscale(String idDebut, String fin, String cours) throws Exception{
         this();
        // Connection conn = BddObject.getPostgreSQL();
@@ -52,30 +51,37 @@ public class FinEscale extends BddObject<DebutEscale>{
             conn.close();
         }
     }
+    
     public String getIdFin() {
         return idFin;
     }
+    
     public void setIdFin(String idFin) {
         this.idFin = idFin;
     }
+    
     public String getIdDebut() {
         return idDebut;
     }
+    
     public void setIdDebut(String idDebut) {
         this.idDebut = idDebut;
     }
+    
     public Timestamp getFin() {
         return fin;
     }
+    
     public void setFin(Timestamp fin) {
         this.fin = fin;
     }
+    
     public double getCours() {
         return cours;
     }
+    
     public void setCours(double cours) {
         this.cours = cours;
     }
-    
-    
+
 }
